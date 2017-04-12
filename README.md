141cae93b2562f61fdd74d99b7285ecdcdf5e3a4
Calvin Shi <sivitas@gmail.com>
2017-04-12 11:52:42 -0700

    handle query params; allow loading 2 different ads for the issues; add README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # videojs-demo
-demo for videojs issues
+
+Hosted at <https://sivitas.github.io/videojs-demo/>
+
+Autoplay and muted video demo with preroll ad using Google IMA SDK
+
+## Current Issues
+1. Clicking the `next` button while ad is playing will load next video but break the ad. Ad will not play on second video and will throw an error on third video.
+  * If you pause then play on the second video, the ad plays
+  * Eventually the ad will 'fix' itself after 2-3 videos from the first 'skip'
+2. Adding the query param `?vpaid=1` will load a vpaid ad instead that does not autoplay on mobile phones
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* globals videojs */
 (function() {
   'use strict';
 
@@ -13,42 +14,68 @@
     prerollTimeout: 3000,
     timeout: 3000
   };
+  var queryParams = {};
 
-  // var adTag = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
-  var adTag = 'https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=14432293&PluID=0&pos=8834&ord=%5Btimestamp%5D&cim=1';
+  var ads = {
+    doubleclick: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=',
+    vpaid: 'https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=14432293&PluID=0&pos=8834&ord=%5Btimestamp%5D&cim=1'
+  };
   var videos = [
     'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
     'http://techslides.com/demos/sample-videos/small.mp4',
     'https://mtc.cdn.vine.co/r/videos/84C4E2C3DD1357938356432023552_574449b2afd.25.1.F040A49F-C8B2-4EC1-8F9F-6525F1365103.mp4?versionId=eweULi8mnxy8zDTuJ4FfkWl4ZeVpOm7r'
     ];
   var videoIndex = 0;
+  var activeAd = ads.doubleclick;
 
   // api
   window.loadNext = loadNext;
   window.loadPrev = loadPrev;
 
-  player.ima(options, _createAdEvents);
-  player.ima.initializeAdDisplayContainer();
-
-  _loadVideo(videos[0], adTag);
+  // init
+  _exctractQueryParams();
+  _createEventHandlers();
+  _init();
 
   // api fns
   function loadNext() {
     videoIndex++;
     if (videoIndex >= videos.length) { videoIndex = 0; }
-    _loadVideo(videos[videoIndex], adTag);
+    _loadVideo(videos[videoIndex], activeAd);
   }
   function loadPrev() {
     videoIndex--;
     if (videoIndex < 0) { videoIndex = videos.length-1; }
-    _loadVideo(videos[videoIndex], adTag);
+    _loadVideo(videos[videoIndex], activeAd);
   }
   // private
-  function _createAdEvents() {
+  function _createAdEventHandlers() {
+    // TODO
     player.ima.startFromReadyCallback();
+  }
+  function _createEventHandlers() {
+    // auto advance to next video on video end
+    player.on('ended', function() {
+      loadNext();
+    });
+  }
+  function _exctractQueryParams() {
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    hashes.forEach(function(hash) {
+      hash = hash.split('=');
+      queryParams[hash[0]] = hash[1];
+    });
   }
   function _loadVideo(video, ad) {
     player.ima.setContentWithAdTag(video, ad, false);
     player.ima.requestAds();
+  }
+  function _init() {
+    if (queryParams.vpaid === '1') { activeAd = ads.vpaid; }
+
+    player.ima(options, _createAdEventHandlers);
+    player.ima.initializeAdDisplayContainer();
+
+    _loadVideo(videos[0], activeAd);
   }
 }());
